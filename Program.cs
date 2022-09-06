e2bbbd648a16fd7ce3da84611350164caf59f0c9
emil.a.johansson <emil.a.johansson@gmail.com>
2022-09-06 11:33:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace CalculatorAssignment_Lexicon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number. Only whole numbers are valid.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your operator. (+, -, *, /, %.");
+            string op = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your second number. Only whole numbers are valid.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result;
+
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine(result);
+                    break;
+                case "%":
+                    result = num1 % num2;
+                    Console.WriteLine(result);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid.");
+                    break;
+            }
+        }
+    }
+}
