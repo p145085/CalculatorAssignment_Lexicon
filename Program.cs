73e575b28986f592b80fed857280ea3414b8c287
emil.a.johansson <emil.a.johansson@gmail.com>
2022-09-07 02:56:05 +0200

    Replaced 'int' values with 'double'.

--- Program.cs
+++ Program.cs
@@ -22,13 +22,13 @@
 
         static void calculator()
         {
-            Console.WriteLine("Enter your first number. Only whole numbers are valid.");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your first number.");
+            double num1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter your operator. (+, -, *, /, %.");
             string op = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("Enter your second number. Only whole numbers are valid.");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            int result;
+            Console.WriteLine("Enter your second number.");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result;
 
             switch (op)
             {
